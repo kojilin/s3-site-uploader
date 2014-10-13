384002fbea4c606815579e662742d8e4050bcab0
kojilin <koji.lin@gmail.com>
2014-10-13 14:29:56 +0800

    dry run flag

--- src/main/scala/s3uploader/Uploader.scala
+++ src/main/scala/s3uploader/Uploader.scala
@@ -11,10 +11,12 @@ object Uploader {
     val config = ConfigFactory.load()
     val client = S3SiteClientImpl
     val root = Paths.get(config.getString("localKeyPath"))
+    val dryRun = if(!config.hasPath("dryRun")) true else config.getBoolean("dryRun")
+    if(dryRun) println(">>>>>>>>dry run") else println(">>>>>>>>>not dry run")
     val result = new MD5Checker(root, client).check
     result.foreach(t => {
       println((if(t._2) "add new file" else "update exist file") + ">>>" + t._1)
-      client.uploadFile(t._1.toString, root.resolve(t._1).toFile.getAbsoluteFile)
+      if(!dryRun) client.uploadFile(t._1.toString, root.resolve(t._1).toFile.getAbsoluteFile)
     })
   }
 }
